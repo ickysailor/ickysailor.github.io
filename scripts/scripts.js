3a353cc48db98bb882cec0ce06d7d7274ffd6b05
ickysailor <ickysailor@gmail.com>
2025-11-07 08:50:06 -0800

    ladeedadee

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,12 +9,9 @@ window.logCustom = function(msg) {
 // Viewport Fix
 // ---------------------------
 function setRealViewportHeight() {
-  const vh = window.visualViewport ? window.visualViewport.height * 0.01 : window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
-window.addEventListener('resize', setRealViewportHeight);
-window.addEventListener('orientationchange', setRealViewportHeight);
-setRealViewportHeight();
 
 // ---------------------------
 // DOM
